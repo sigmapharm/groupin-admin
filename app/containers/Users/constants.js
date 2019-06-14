8a25d9dcce2899659bff507a3e671aac39f70834
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2019-06-14 18:50:37 +0100

    fix user added twice

--- app/containers/Users/constants.js
+++ app/containers/Users/constants.js
@@ -4,6 +4,6 @@ export const PUT_USERS_LIST_ACTION = 'app/users/putList';
 export const SUBMIT_CREATE_USER = 'app/users/SUBMIT_CREATE_USER';
 export const MANAGE_CREATE_USER_RESPONSE =
   'app/users/MANAGE_CREATE_USER_RESPONSE';
-export const SUBMIT_UPDATE_USER = 'app/users/SUBMIT_CREATE_USER';
+export const SUBMIT_UPDATE_USER = 'app/users/SUBMIT_UPDATE_USER';
 export const MANAGE_UPDATE_USER_RESPONSE =
   'app/users/MANAGE_UPDATE_USER_RESPONSE';
