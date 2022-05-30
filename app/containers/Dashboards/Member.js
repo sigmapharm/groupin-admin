e0cdc4b13751bd8195a7ab8c34411fa2ce5e0f39
unknown <sabmourad4@gmail.com>
2022-05-30 11:25:47 +0100

    fix charts

--- app/containers/Dashboards/Member.js
+++ app/containers/Dashboards/Member.js
@@ -9,6 +9,7 @@ import { getStatistics } from './actions';
 import Card from './Card';
 import { selectStatistics } from './selectors';
 import PieChart from './charts/PieChart';
+import BarChart from './charts/BarChart';
 import SmallCard from './SmallCard';
 import { formatNumber } from '../../utils/formatNumber';
 
@@ -17,6 +18,9 @@ const Member = ({ classes, dispatch, statistics, userRole }) => {
     dispatch(getStatistics(userRole));
   }, []);
   console.log('statistics', statistics);
+
+  const sortedChars = _.sortBy(statistics.caparLabo ? statistics.caparLabo : [], ['value'], ['desc']);
+
   return (
     <div className={classes.root}>
       <div>
@@ -53,10 +57,16 @@ const Member = ({ classes, dispatch, statistics, userRole }) => {
             </div>
             <div style={{ marginBottom: '24px' }} />
             <div className={classes.container}>
-              <PieChart
-                data={statistics.caparArticle ? statistics.caparArticle.map(v => v.value) : []}
-                labels={statistics.caparArticle ? statistics.caparArticle.map(v => v.name) : []}
-                label="Chiffre d'affaire par article(%)"
+              <BarChart
+                data={sortedChars
+                  .map(v => v.value)
+                  .slice(0, 10)
+                  .reverse()}
+                labels={sortedChars
+                  .map(v => v.name.slice(0, 10))
+                  .slice(0, 10)
+                  .reverse()}
+                label="Top 10 articles commandés (MAD)"
               />
             </div>
           </Grid>
